8547b24e82f7e209c72b6410e5a001f92d6e88b5
ivan armagnat <ivan.armagnat@gmail.com>
2017-01-31 17:26:59 +0100

    Players class done, event listner mindwrecked me so...  Pending

--- renderer.js
+++ renderer.js
@@ -13,6 +13,94 @@ var highScore
 
 var $ = require('jquery');
 
+function Players (startPos, body, keys) {
+
+    this.length = 3;
+    this.alive = true;
+    this.xDir = 0;
+    this.yDir = 1;
+    this.prevxDir = 0;
+    this.prevyDir = 1;
+    this.xpos = startPos[0];
+    this.ypos = startPos[1];
+
+    this.up = keys[0];
+    this.down = keys[1];
+    this.left = keys[2];
+    this.right = keys[3];
+
+    this.xBody= [];
+    this.yBody= [];
+
+    //DO NOT FORGET array.unshift
+
+    this.xBody.push(body[0][0]);
+    this.yBody.push(body[0][1]);
+
+    this.xBody.push(body[1][0]);
+    this.yBody.push(body[1][1]);
+
+    this.move = function (){
+
+        nextX = (this.xDir + this.xpos + size)%size;
+        nextY = (this.yDir + this.ypos + size)%size;
+
+        this.xBody.unshift(this.xpos)
+        this.yBody.unshift(this.ypos)
+
+        this.xBody.splice(this.length - 1, 10)
+        this.yBody.splice(this.length - 1, 10)
+
+        this.xpos = nextX
+        this.ypos = nextY
+
+        this.prevxDir = this.xDir;
+        this.prevyDir = this.yDir;
+
+        if (nextX in this.xBody && nextY in this.yBody){
+            this.alive = false
+            return false
+        }
+
+        return true
+    }
+
+    this.eat = function(){
+        this.length ++
+    }
+
+    this.updateDirection = function(key){
+        
+        if(key == this.left) {
+            if (this.prevxDir != 1 || this.prevyDir != 0){
+                this.xDir = -1
+                this.yDir = 0
+            }
+        } else if(key == this.right) {
+            if (this.prevxDir != -1 || this.prevyDir != 0){
+                this.xDir = 1
+                this.yDir = 0
+            }
+        } else if(key == this.down) {
+            if (this.prevxDir != 0 || this.prevyDir != 1){
+                this.xDir = 0
+                this.yDir = -1
+            }
+        } else if(key == this.up) {
+            if (this.prevxDir != 0 || this.prevyDir != -1){
+                this.xDir = 0
+                this.yDir = 1
+                console.log('pwet')
+            }
+        }
+    }
+
+
+};
+
+
+var Player1 = new Players([0, 2], [ [0, 1], [0, 0] ], [38, 40, 37, 39])
+
 
 function newStage(){
     $("#stage").empty();
@@ -138,32 +226,20 @@ function play(map){
 
 }
 
+
+/*
+document.addEventListener('keydown', function updateUp1( event ) {
+    Player1.up = event.keyCode
+    console.log(Player1.up)
+} , true);
+
+document.removeEventListener('keydown', updateUp1(event) , true )
+*/
+
+// event listner: keydown -> P1 direction  udate
 document.addEventListener('keydown', function(event) {
-    //left
-    if(event.keyCode == 37) {
-        if (prevDirection[0] != 1 && prevDirection[1] != 0){
-            direction = [-1, 0]
-        }
-    }
-    //right
-    else if(event.keyCode == 39) {
-        if (prevDirection[0] != -1 && prevDirection[1] != 0){
-            direction = [1, 0]
-        }
-    }
-    //down
-    else if(event.keyCode == 40) {
-        if (prevDirection[0] != 0 && prevDirection[1] != 1){
-            direction = [0, -1]
-        }
-    }
-    //up
-    else if(event.keyCode == 38) {
-        if (prevDirection[0] != 0 && prevDirection[1] != -1){
-            direction = [0, 1]
-        }
-    }
-});
+    Player1.updateDirection(event.keyCode)
+}, false);
 
 $('#start').on('click', function(event){
 
@@ -225,7 +301,5 @@ $( document ).ready(function() {
     $("#x0y0").html('<i class="fa fa-arrow-up"></i>')
 
     updateScreen(map);
-
-
 
 });
